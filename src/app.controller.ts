532bafc6a5b4efefa146c78267140f9d9bc73f88
PfefferBotond <112927137+PfefferBotond@users.noreply.github.com>
2023-01-13 09:41:06 +0100

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import {
+  Body,
   Controller,
   Get,
+  Post,
+  Query,
+  Redirect,
   Render,
 } from '@nestjs/common';
 import { AppService } from './app.service';
+import Book from './book.tdo';
 import db from './db';
 
 @Controller()
@@ -12,7 +17,38 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  async index(@Query('rating') rating: number) {
+    const [rows]: any = await db.execute('SELECT * FROM books')
+
+    let books = rows as Book[];
+
+    if(rating != null) {
+      books = books.filter(book => book.rating == rating)
+    }
+
+    books.sort((a,b) => b.rating-a.rating)
+
+    return {
+      books: books
+    }
+
+  }
+
+  @Get('/books/new')
+  @Render('newBook')
+  newBookForm() {
+    return {
+
+    }
+  }
+
+  @Post('/books/new')
+  @Redirect()
+  async addNewBook(@Body() book: Book) {
+    console.log(book)
+    await db.execute('INSERT INTO books (title, rating) VALUES (?, ?)', [book.title, book.rating]);
+    return {
+      url: '/'
+    }
   }
 }
